7f7453e6fa69521e36b335028faaf8315687c461
Raminder Singh <raminder.singh@spirent.com>
2024-06-24 13:18:23 -0700

    all template mvc piece

---
+++ controller/static.go
@@ -0,0 +1,13 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/raminderis/lenslocked/views"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+}
--- main.go
+++ main.go
@@ -2,58 +2,35 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/raminderis/lenslocked/controller"
+	"github.com/raminderis/lenslocked/views"
 )
-
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	t, err := template.ParseFiles(filepath)
-	if err != nil {
-		log.Printf("Parsing template error: %v", err)
-		http.Error(w, "There was error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, nil)
-	if err != nil {
-		log.Printf("Execution template error: %v", err)
-		http.Error(w, "There was error executing the template.", http.StatusInternalServerError)
-		return
-	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tpath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tpath)
-}
-
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-	userID := chi.URLParam(r, "userID")
-	w.Write([]byte(fmt.Sprintf("hi %v", userID)))
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tpath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tpath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tpath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tpath)
-}
 
 func main() {
 	r := chi.NewRouter()
-	r.With(middleware.Logger).Get("/users/{userID}", usersHandler)
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controller.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controller.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controller.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	})
@@ -63,7 +40,7 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	err := http.ListenAndServe(":"+port, r)
+	err = http.ListenAndServe(":"+port, r)
 	if err != nil {
 		panic(err)
 	}
