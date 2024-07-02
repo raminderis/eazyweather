03ca4c0ab83eca65604dbce37330a515c55ae484
Raminder Singh <raminder.singh@spirent.com>
2024-07-02 15:11:19 -0700

    sql updates

--- models/sql/sessions.sql
+++ models/sql/sessions.sql
@@ -3,9 +3,3 @@ CREATE TABLE IF NOT EXISTS sessions (
     user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE,
     token_hash TEXT UNIQUE NOT NULL
 );
-
-
-INSERT INTO sessions (user_id, token_hash)
-VALUES (1, 'xyz-123') ON CONFLICT (user_id) DO
-UPDATE
-SET token_hash='xyz-123';
